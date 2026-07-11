49b02bb31859cdbd2d3e57099f72c6733fd51c8e
aisuki-fa <faisallegendssghsc25@gmail.com>
2026-07-11 16:20:38 +0600

    revert: remove test code from main.c

--- src/main.c
+++ src/main.c
@@ -1,22 +1,13 @@
 #include "raylib.h"
 #include "cascade.h"
-#include "input.h"
 
 int main(void) {
     InitWindow(WINDOW_W, WINDOW_H, "Cascade - SPH Fluid Simulator");
     SetTargetFPS(60);
-
-    SimState sim = {0};
-    sim.count = 0;
-    sim.paused = false;
-
     while (!WindowShouldClose()) {
-        Vector2 mouse = GetMousePosition();
-        input_update(&sim, NULL, NULL, mouse, GetFrameTime());
-
         BeginDrawing();
         ClearBackground((Color){7, 9, 26, 255});
-        DrawText(TextFormat("Count: %d", sim.count), 10, 10, 20, WHITE);
+        DrawText("Cascade loading...", 600, 380, 24, WHITE);
         EndDrawing();
     }
     CloseWindow();

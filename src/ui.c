769a576c58a8a726508f778522e857a1d95b7836
aisuki-fa <faisallegendssghsc25@gmail.com>
2026-07-12 15:38:09 +0600

    Ui sidebar addition and testing

--- src/ui.c
+++ src/ui.c
@@ -1,9 +1,11 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "cascade.h"
 #include "ui.h"
+#include "obstacles.h"
 
 void ui_draw_sidebar(SimState* sim, UIState* ui, ObstacleList* obs) {
-    (void)sim;
-    (void)ui;
-    (void)obs;
+    DrawRectangle(0, 0, SIDEBAR_W, WINDOW_H, (Color){155, 82, 119, 255});
+    DrawLine(SIDEBAR_W, 0, SIDEBAR_W, WINDOW_H, (Color){25, 40, 72, 255});
+    DrawText("CASCADE", 30, 15, 32, (Color){0, 0, 0, 255});
 }

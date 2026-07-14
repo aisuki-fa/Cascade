9d4e4d336c076e9c5a4f182d3a071796b9b43534
kyatoT <takia09890@gmail.com>
2026-07-14 23:31:46 +0600

    changed colours and added ongoing status

--- src/renderer.c
+++ src/renderer.c
@@ -31,14 +31,14 @@ void render_velocity(SimState* sim){
     for(int i=0;i<sim->count;i++){
         float speed=sqrt(sim->particles[i].vel.x*sim->particles[i].vel.x+sim->particles[i].vel.y*sim->particles[i].vel.y);
         if (speed<100)
-            sim->particles[i].color=lerp_color((Color){0,0,80}, (Color){135,206,235}, (speed)/100);
+            sim->particles[i].color=lerp_color((Color){213, 196, 166}, (Color){114,43,106}, (speed)/100);
         else if(speed<=400)
-            sim->particles[i].color=lerp_color((Color){135,206,235}, (Color){255,255,0}, (speed-100)/300);
+            sim->particles[i].color=lerp_color((Color){114,43,106}, (Color){162,38,75}, (speed-100)/300);
         else if(speed>400){
-            speed=(speed-400)/200;
-            if(speed>1)
-            speed=1;
-            sim->particles[i].color=lerp_color((Color){255,255,0}, (Color){255,0,0}, speed);
+            speed=(speed-400)/400;
+            // if(speed>1)
+            // speed=1;
+            sim->particles[i].color=lerp_color((Color){162,38,75}, (Color){211,33,45}, speed);
     }
     DrawCircleV(sim->particles[i].pos,sim->particle_radius,sim->particles[i].color);
     }
@@ -46,12 +46,12 @@ void render_velocity(SimState* sim){
 void render_density(SimState* sim){
     for(int i=0;i<sim->count;i++){
         float t= sim->particles[i].density / sim->target_density;
-        t=fminf(fmaxf(t,0),2);
+        // t=fminf(fmaxf(t,0),2);
         if(t<1){
-            sim->particles[i].color= lerp_color((Color){0,0,80}, (Color){100,150,255}, t);
+            sim->particles[i].color= lerp_color((Color){200,186,126}, (Color){78, 80, 38}, t);
         }
         else{
-            sim->particles[i].color= lerp_color((Color){100, 150, 255}, WHITE, t - 1);
+            sim->particles[i].color= lerp_color((Color){73, 57, 44},(Color){78, 80, 38},  1/t);
             
         }
         DrawCircleV(sim->particles[i].pos,sim->particle_radius,sim->particles[i].color);
@@ -65,12 +65,15 @@ void render_density(SimState* sim){
 void render_hud(int count, int fps, bool paused) {
     (void)count;
     Color fc = fps > 50 ? GREEN : fps > 30 ? YELLOW : RED;
-    DrawText(TextFormat("FPS: %d", fps), WINDOW_W - 90, 12, 16, fc);
+    DrawText(TextFormat("FPS: %d", fps), WINDOW_W - 120, 12, 30, fc);
     if (paused) {
-        DrawRectangle(SIDEBAR_W, 0, SIM_W, WINDOW_H, (Color){0, 0, 0, 100});
+        DrawRectangle(SIDEBAR_W, 0, SIM_W, WINDOW_H, (Color){251, 199, 191, 100});
         const char* msg = "|| PAUSED ||";
         int w = MeasureText(msg, 28);
         DrawText(msg, SIDEBAR_W + (SIM_W - w) / 2, WINDOW_H / 2 - 14, 28, (Color){255, 255, 255, 200});
+    }
+    else{
+        DrawText(TextFormat("Ongoing"), WINDOW_W - 120, 45, 30, fc);
     }
 }
 
@@ -95,7 +98,7 @@ void render_blended(SimState* sim,SpatialHash* sh){
         for(int j=0;j<out_count;j++){
             int id=out_ids[j];
             float distance= sqrt(pow(sim->particles[i].pos.x-sim->particles[id].pos.x,2)+pow(sim->particles[i].pos.y-sim->particles[id].pos.y,2));
-            float wt= fmaxf(0,1.0f-distance/30.0f);
+            float wt= fmaxf(0,1.0f-distance/300.0f);
             r+=sim->particles[id].color.r*wt;
             g+=sim->particles[id].color.g*wt;
             b+=sim->particles[id].color.b*wt;
@@ -106,8 +109,5 @@ void render_blended(SimState* sim,SpatialHash* sh){
             c=(Color){(int)(r/w),(int)(g/w),(int)(b/w),255};
             else c=sim->particles[i].color;
             DrawCircleV(sim->particles[i].pos,sim->particle_radius,c);
-
-
-        
     }
 }

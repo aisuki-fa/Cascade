1362d704da9160aeb811a347e80fcaa568fc7229
aisuki-fa <faisallegendssghsc25@gmail.com>
2026-07-14 20:35:30 +0600

    Comments for ui.h

--- src/ui.h
+++ src/ui.h
@@ -1,9 +1,10 @@
-#ifndef UI_H
-#define UI_H
+#ifndef UI_H                                           // include guard
+#define UI_H                                           // mark this header as included
 
-#include "cascade.h"
-#include "obstacles.h"
+#include "cascade.h"                                   // SimState, UIState types
+#include "obstacles.h"                                 // ObstacleList and such
 
+// Draws the entire left sidebar: title, controls, section backgrounds, particle counter
 void ui_draw_sidebar(SimState* sim, UIState* ui, ObstacleList* obs);
 
-#endif
+#endif                                                 // end of include guard

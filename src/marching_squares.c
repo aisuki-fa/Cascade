6bea97bc2d175826fc5959f4082fd2ed31896bdd
kyatoT <takia09890@gmail.com>
2026-09-13 19:23:31 +0600

    added changes

--- src/marching_squares.c
+++ src/marching_squares.c
@@ -66,7 +66,7 @@ void ms_draw(float field[MS_W][MS_H], Color color){
                 default: break;
             }        
 
-    void ms_clear_field(float field[MS_H][MS_W]){
+    void ms_clear_field(float field[MS_W][MS_H]){
         for(int x=0;x<MS_W;x++){
             for(int y=0;y<MS_H;y++){
                 field[x][y]=0.0f;
